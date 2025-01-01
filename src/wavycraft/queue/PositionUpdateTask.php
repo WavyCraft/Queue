06e17a9eff342a02d3502dadf4f3f600b0398bb1
WavyCraft <wavycraftnetwork@gmail.com>
2024-12-31 18:14:55 -0800

    Update PositionUpdateTask.php

--- src/wavycraft/queue/PositionUpdateTask.php
+++ src/wavycraft/queue/PositionUpdateTask.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace wavycraft\queue;
 
 use pocketmine\scheduler\Task;
+
+use pocketmine\utils\Config;
+
 use pocketmine\Server;
 
 class PositionUpdateTask extends Task {
@@ -13,7 +16,10 @@ class PositionUpdateTask extends Task {
         foreach (Server::getInstance()->getOnlinePlayers() as $player) {
             $position = QueueManager::getQueuePosition($player);
             if ($position > 0) {
-                $player->sendTip("You are #$position in the queue...");
+                $messages = new Config(Loader::getInstance()->getDataFolder() . "messages.yml", Config::YAML);
+                $msg = $messages->get("queue_position");
+                $msg = str_replace("{position}", (string)$position, $msg);
+                $player->sendTip($msg);
             }
         }
         QueueManager::processQueue();
